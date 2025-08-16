ae52503a400a18ed41de42977b4235b97ef3e1d6
Marques Traylor <traylorboy@protonmail.com>
2025-08-16 08:24:20 -0500

    event tests

--- recordboy.py
+++ recordboy.py
@@ -11,6 +11,7 @@ class RecordBoy:
             path (str): The file path for the recorded events.
         """
         self.path = path
+        self.events = []
 
     def get_path(self) -> str:
         """Get the current file path.
@@ -35,3 +36,56 @@ class RecordBoy:
         pass
 
     # Listeners for mouse and keyboard events
+    def on_move(self, x, y):
+        """Mouse movement event.
+
+        Args:
+            x (int): The x-coordinate of the mouse pointer.
+            y (int): The y-coordinate of the mouse pointer.
+        """
+        self.events.append(("move", x, y))
+
+    def on_click(self, x, y, button, pressed):
+        """Mouse click event.
+
+        Args:
+            x (int): The x-coordinate of the mouse pointer.
+            y (int): The y-coordinate of the mouse pointer.
+            button (Button): The button that was clicked.
+            pressed (bool): Whether the button was pressed or released.
+        """
+        self.events.append(("click", x, y, button.name, pressed))
+
+    def on_scroll(self, x, y, dx, dy):
+        """Mouse scroll event.
+
+        Args:
+            x (int): The x-coordinate of the mouse pointer.
+            y (int): The y-coordinate of the mouse pointer.
+            dx (int): The amount scrolled in the x-direction.
+            dy (int): The amount scrolled in the y-direction.
+        """
+        self.events.append(("scroll", x, y, dx, dy))
+
+    def on_press(self, key):
+        """Key press event.
+
+        Args:
+            key (keyboard.key): Key pressed.
+        """
+        try:
+            self.events.append(("key_press", key.char))
+        except AttributeError:
+            self.events.append(("key_press", str(key)))
+
+    def on_release(self, key):
+        """Key release event.
+
+        Args:
+            key (keyboard.key): Key released.
+        """
+        self.events.append(("key_release", str(key)))
+
+        # Esc stops listeners
+        if key == keyboard.Key.esc:
+            return False
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from recordboy import RecordBoy
+from pynput import mouse, keyboard
 
 
 def test_init():
@@ -20,7 +21,24 @@ def test_getter_setter():
     print("Getter and setter test passed.")
 
 
+def test_events():
+    print("Testing mouse and keyboard events...")
+
+    recordboy = RecordBoy(path="test_events.json")
+    key = keyboard.Key.space
+
+    recordboy.on_move(100, 200)
+    recordboy.on_click(100, 200, mouse.Button.left, True)
+    recordboy.on_scroll(100, 200, 0, -1)
+    recordboy.on_press(key)
+    recordboy.on_release(key)
+    assert len(recordboy.events) == 5
+
+    print("Event tests passed")
+
+
 if __name__ == "__main__":
     print("Running tests...")
     test_init()
     test_getter_setter()
+    test_events()
